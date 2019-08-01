3af37738031e040756ff9872806d0a3cf1120fce
Duy Vo <duy.vo@forte.net>
2019-08-01 12:48:46 -0500

    Add connect file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# nodejs-sql-connect
+# Node.js SQL Connect
+
+## Get started guide (Windows)
 
 https://www.microsoft.com/en-us/sql-server/developer-get-started/node/windows/step/2.html
--- connect.js
+++ connect.js
@@ -4,16 +4,16 @@ var async = require("async");
 
 // Create connection to database
 var config = {
-  server: "localhost",
+  server: "<server>", // update me
   authentication: {
     type: "default",
     options: {
-      userName: "sa", // update me
-      password: "your_password" // update me
+      userName: "<username>", // update me
+      password: "<password>" // update me
     }
   },
   options: {
-    database: "SampleDB"
+    database: "<database>" // update me
   }
 };
 
@@ -23,28 +23,22 @@ function Read(callback) {
   console.log("Reading rows from the Table...");
 
   // Read all rows from table
-  request = new Request(
-    "SELECT Id, Name, Location FROM TestSchema.Employees;",
-    function(err, rowCount, rows) {
-      if (err) {
-        callback(err);
-      } else {
-        console.log(rowCount + " row(s) returned");
-        callback(null);
-      }
+  request = new Request("<select_statement>;", (err, rowCount, rows) => {
+    if (err) callback(err);
+    else {
+      console.log(rowCount + " row(s) returned");
+      callback(null);
     }
-  );
+  });
 
   // Print the rows read
   var result = "";
-  request.on("row", function(columns) {
-    columns.forEach(function(column) {
-      if (column.value === null) {
-        console.log("NULL");
-      } else {
-        result += column.value + " ";
-      }
+  request.on("row", columns => {
+    columns.forEach(column => {
+      if (column.value === null) console.log("NULL");
+      else result += column.value + " ";
     });
+
     console.log(result);
     result = "";
   });
@@ -54,18 +48,14 @@ function Read(callback) {
 }
 
 function Complete(err, result) {
-  if (err) {
-    callback(err);
-  } else {
-    console.log("Done!");
-  }
+  if (err) callback(err);
+  else console.log("Done!");
 }
 
 // Attempt to connect and execute queries if connection goes through
-connection.on("connect", function(err) {
-  if (err) {
-    console.log(err);
-  } else {
+connection.on("connect", err => {
+  if (err) console.log(err);
+  else {
     console.log("Connected");
 
     // Execute all functions in the array serially
